5ab5a4af15df62e82b59bb61a17a32f0eef2759b
swapnil.s <swapnil.s@mmmdvswapniltemp.internal.directi.com>
2019-10-08 13:17:38 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      'sans': ['-apple-system', 'BlinkMacSystemFont'],
+      'serif': ['Georgia', 'Cambria'],
+      'mono': ['SFMono-Regular', 'Menlo'],
+      'display': ['Nunito']
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
